bae50ed69cb6c8e03ad271e04652083e54848f79
Elad Ahdut <77791369+eladahdut@users.noreply.github.com>
2021-11-23 13:33:37 +0200

    deployed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import NavigationBar from './components/navbar/NavigationBar.tsx';
-import MoviesList from './components/moviesList/MoviesList';
-import MoviesDataProvider from './context/moviesContext'
+import NavigationBar from "./components/navbar/NavigationBar.tsx";
+import MoviesList from "./components/moviesList/MoviesList";
+import MoviesDataProvider from "./context/moviesContext";
 import Box from "@mui/material/Box";
-import './styles.scss';
+import "./styles.scss";
 
 function App() {
   return (
@@ -12,10 +12,10 @@ function App() {
         sx={{
           display: "flex",
           justifyContent: "center",
-        }}>
-        <h1
-          style={{ margin: "1rem 0 0.25rem 0", color: "rgb(117, 16, 247)" }}>
-          Movies List
+        }}
+      >
+        <h1 style={{ margin: "1rem 0 0.25rem 0", color: "rgb(117, 16, 247)" }}>
+          Movies
         </h1>
       </Box>
       <hr style={{ width: "2%", border: "1px solid rgb(117 16 247 / 70%)" }} />
